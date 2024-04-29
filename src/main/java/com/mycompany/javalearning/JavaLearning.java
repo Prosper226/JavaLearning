1f7b96a5de8b3998c5dfd057df82e58549162c62
Prosper226 <psedgo@gmail.com>
2024-04-29 12:57:22 +0000

    Ajout de la fonctionnalié de calcul de superficie d'un rectangle

--- src/main/java/com/mycompany/javalearning/JavaLearning.java
+++ src/main/java/com/mycompany/javalearning/JavaLearning.java
@@ -13,4 +13,15 @@ public class JavaLearning {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    
+    /**
+     * Methode pour calculer la superficie d'un rectangle.
+     * @param longueur Longueur du rectangle
+     * @param largeur Largeur du rectangle
+     * @return La superficie du rectangle
+     */
+    public float calculerSuperficerRectangle(final float longueur, final float largeur){
+        return longueur * largeur;
+    }
+    
 }
